8a78e87e676a798535551a987944d53bf3548fd4
Jalilur Rahman <jrahman90.work@gmail.com>
2022-01-07 15:09:43 -0500

    Initial Commit

---
+++ first-game/first_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_gameApp.swift
+//  first-game
+//
+//  Created by Jalilur Rahman on 1/7/22.
+//
+
+import SwiftUI
+
+@main
+struct first_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
